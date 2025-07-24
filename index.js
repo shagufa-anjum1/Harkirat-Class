0468b7eebcab7a45c2d68e15f506a9988e08626e
shagufa-anjum1 <shagufa.anjum2021@vitbhopal.ac.in>
2025-07-24 11:57:01 +0530

    Axios Function

--- index.js
+++ index.js
@@ -2,17 +2,52 @@
 const axios = require('axios')
 
 async function main(){
-  const response = await fetch('/', {
-    method: "POST"
+  const respone = await fetch('/', {
+    method: "GET",
+    body:{
+      userName: "shagufa",
+      password: "1233"
+    },
+    
+    headers:{
+      "Authorization": "shagufa 123,"
+    },
   });
-  const json = await response.json();
-  console.log(json.todos.length);
+
+  const json = await respone.json();
+  console.log(json.todo.length);
+
 }
 
 
-async function main() {
-  const response = await axios.get('/')
-  console.log(response.todos.length)
-  
+async function axiFn(){
+  const rrsponse = await axios.get('/')
+  console.log(respone.todo.length);
 }
+
+async function axiosFn(){
+ const respone = await axios.post('/',
+   {
+  userName: "sofi",
+  password: "hjvk"
+ },
+ {
+  headers: {
+    "Authorization": "sofi like"
+  },
+ },
+);
+console.log(respone.data);
+}
+
+axios({
+  method: 'post',
+  url: '/',
+  data: {
+    firstN: "sofi",
+    lastN: "khan"
+  }
+});
+
+
 
